fc35e594a7abe22a469f0d6e40294ff19fb1a857
Raphael Moral Piazera <80533929+raphamoral@users.noreply.github.com>
2024-06-21 20:10:29 -0300

    addding gitignore

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3fe&y2dqsp2vk10$-!s58*2u*=stty^pu=1ygkoset)ezkk2x_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
